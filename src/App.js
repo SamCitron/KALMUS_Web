d6eba095f34c3007a4898bf4653cabaf3027e49b
Sam Citron <sec038@bucknell.edu>
2022-05-06 14:42:01 -0400

    updating movie search

--- src/App.js
+++ src/App.js
@@ -1,30 +1,22 @@
 import React, { useState } from 'react';
 import { Test } from './components/Test';
 import Dropdown from './components/Dropdown';
+const fs = require('fs');
 
 function App(props) {
-  const [imageFile, setImageFile] = useState('');
-  const [image, setImage] = useState({});
+  const [image, setImage] = useState();
 
   function validateResponse(response, movieLink) {
-    console.log(response.type);
     if (!response.ok | (response.type == 'opaque')) {
       console.log('res bad, restart timer');
-      setTimeout(getImage(movieLink), 1000);
+      return new Promise(function (resolve) {
+        setTimeout(() => {
+          resolve(getImage(movieLink));
+        }, 1000);
+      });
     } else {
-      console.log('GET RES:', response.type);
-      console.log('res should be here');
+      console.log('res is good, returning response');
       return response;
-    }
-  }
-
-  function checkBlob(response) {
-    response.blob();
-    if (blob.size == 0) {
-      console.log('blob 0, try again');
-      setTimeout(checkBlob(response), 1000);
-    } else {
-      return response.blob();
     }
   }
 
@@ -37,79 +29,17 @@ function App(props) {
       },
     })
       .then((response) => validateResponse(response, movieLink))
-      .then((response) => checkBlob(response))
+      .then((response) => response.blob())
       .then((blob) => {
         let url = URL.createObjectURL(blob);
-        console.log(url);
-        setImage({ pic: url });
+        setImage(url);
       })
       .catch((err) => {
         console.log(err);
       });
   }
 
-  // function getImage(movieLink) {
-  //   fetch('http://localhost:5000/static/' + movieLink, {
-  //     method: 'GET',
-  //     mode: 'cors',
-  //     headers: {
-  //       'Content-Type': 'arraybuffer',
-  //     },
-  //   })
-  //     .then((res) => {
-  //       console.log('GET Movie:', res);
-  //       if (!res.ok) {
-  //         console.log('res bad, restart timer');
-  //         setTimeout(getImage(movieLink), 1000);
-  //       } else {
-  //         console.log('Res good');
-
-  //         // https://developer.mozilla.org/en-US/docs/Web/API/Streams_API/Using_readable_streams
-
-  //         const reader = res.body.getReader();
-  //         return new ReadableStream({
-  //           start(controller) {
-  //             return pump();
-  //             async function pump() {
-  //               const { done, value } = await reader.read();
-  //               // When no more data needs to be consumed, close the stream
-  //               if (done) {
-  //                 controller.close();
-  //                 console.log('Stream Complete');
-  //                 return;
-  //               }
-  //               // Enqueue the next data chunk into our target stream
-  //               controller.enqueue(value);
-  //               return pump();
-  //             }
-  //           },
-  //         });
-  //         // const base64 = btoa(
-  //         //   new Uint8Array(res.data).reduce(
-  //         //     (data, byte) => data + String.fromCharCode(byte),
-  //         //     ''
-  //         //   )
-  //         //);
-  //         //const imageObjectUrl = URL.createObjectURL(res);
-  //         //console.log(base64);
-  //         //setImage({ pic: 'data:;base64,' + base64 });
-  //         //console.log(image.pic);
-  //       }
-  //     })
-  //     .then((stream) => new Response(stream))
-  //     .then((response) => response.blob())
-  //     .then((blob) => URL.createObjectURL(blob))
-  //     .then((url) => {
-  //       console.log(url);
-  //       setImage({ pic: url });
-  //     })
-  //     .catch((err) => {
-  //       console.log(err);
-  //     });
-  // }
-
-  function apphandleDropDown(userSelection) {
-    console.log(userSelection);
+  function generateImage(userSelection) {
     let params = {
       method: 'POST',
       headers: { 'Content-Type': 'application/json' },
@@ -122,20 +52,30 @@ function App(props) {
       .then((res) => res.json())
       .then((res) => {
         console.log('POST RES:', res.result);
-        setImageFile(res.result);
-        // Start timer to go off in 500 seconds
-        setTimeout(getImage(res.result), 1000);
+        // Start timer to go off in 2 second
+        setTimeout(getImage(res.result), 2000);
       })
       .catch((err) => {
         console.log(err);
       });
   }
 
+  function getImageFile(userSelections) {
+    console.log(userSelections);
+
+    const dir = '../backend/json_barcodes';
+    const files = fs.readdirSync(dir);
+
+    for (const file of files) {
+      console.log(file);
+    }
+  }
+
   return (
     <div className='App'>
       <Test />
-      <Dropdown apphandleDropDown={apphandleDropDown} />
-      <img src={image.pic}></img>
+      <Dropdown getImageFile={getImageFile} />
+      <img src={image}></img>
     </div>
   );
 }
--- src/components/Dropdown.js
+++ src/components/Dropdown.js
@@ -1,46 +1,99 @@
 import React, { useState, useEffect } from 'react';
 import { Button } from '@mui/material';
-import Select from 'react-select';
+import {
+  Autocomplete,
+  TextField,
+  InputLabel,
+  MenuItem,
+  FormControl,
+  Select,
+} from '@mui/material';
 
-const options = [
-  {
-    value: '1996_03_mission_impossible.mp4_Average_Background_Color.json',
-    label: 'Mission Impossible Average Background Color',
-  },
-  {
-    value: '1996_03_mission_impossible.mp4_Average_Foreground_Color.json',
-    label: 'Mission Impossible Average Foreground Color',
-  },
-  {
-    value:
-      '1996_03_mission_impossible.mp4_Average_High_contrast_region_Color.json',
-    label: 'Mission Impossible High Contrast Region Color',
-  },
-  {
-    value:
-      '1996_03_mission_impossible.mp4_Average_Low_contrast_region_Color.json',
-    label: 'Mission Impossible Low Contrast Region Color',
-  },
-  {
-    value: '1996_03_mission_impossible.mp4_Average_Whole_frame_Color.json',
-    label: 'Mission Impossible Average Whole Frame Color',
-  },
+const movies = [
+  { name: 'Mission Impossible', value: 'mission_impossible' },
+  { name: 'Ratatouille', value: 'ratatouille' },
+  { name: 'Chungking Express', value: 'Chungking_Express' },
+  { name: 'I Am Love', value: 'I_Am_Love' },
+  { name: 'i Robot', value: 'i_robot' },
+  { name: 'Playtime', value: 'Playtime' },
+  { name: 'Vergigo', value: 'Vertigo' },
 ];
 
 export default function Dropdown(props) {
-  const [choice, setChoice] = useState({});
-
-  function handleDropDown(choice) {
-    setChoice(choice.value);
-    console.log('choice selected: ', choice);
-  }
+  const [movie, setMovie] = useState(null);
+  const [colorMetric, setColorMetric] = useState('');
+  const [frameType, setFrameType] = useState('');
 
   return (
     <div>
-      <Select options={options} onChange={handleDropDown} />
+      <Autocomplete
+        id='select-movie'
+        options={movies}
+        sx={{ width: 500 }}
+        renderInput={(params) => (
+          <TextField {...params} label='Search For A Film' />
+        )}
+        getOptionLabel={(option) => option.name}
+        value={movie}
+        onChange={(_event, newMovie) => {
+          console.log(newMovie);
+          setMovie(newMovie);
+        }}
+      />
+
+      <FormControl fullWidth>
+        <InputLabel id='select-color-metric'>Select Color Metric</InputLabel>
+        <Select
+          labelId='select-color-metric'
+          id='select-color-metric'
+          sx={{ width: 400 }}
+          value={colorMetric}
+          label='Color Metric'
+          onChange={(event) => {
+            console.log(event.target.value);
+            setColorMetric(event.target.value);
+          }}
+        >
+          <MenuItem value='Average'>Average</MenuItem>
+          <MenuItem value='Median'>Median</MenuItem>
+          <MenuItem value='Mode'>Mode</MenuItem>
+          <MenuItem value='Top-dominant'>Top Dominant</MenuItem>
+          <MenuItem value='Weighted-dominant'>Weighted Dominant</MenuItem>
+          <MenuItem value='Bright'>Bright</MenuItem>
+          <MenuItem value='Brightest'>Brightest</MenuItem>
+        </Select>
+      </FormControl>
+
+      <FormControl fullWidth>
+        <InputLabel id='select-frame-type'>Select Frame Type</InputLabel>
+        <Select
+          labelId='select-frame-type'
+          id='select-frame-type'
+          sx={{ width: 400 }}
+          value={frameType}
+          label='Frame Type'
+          onChange={(event) => {
+            console.log(event.target.value);
+            setFrameType(event.target.value);
+          }}
+        >
+          <MenuItem value='Whole_frame'>Whole Frame</MenuItem>
+          <MenuItem value='Low_contrast_region'>Low Contrast Region</MenuItem>
+          <MenuItem value='High_contrast_region'>High Contrast Region</MenuItem>
+          <MenuItem value='Foreground'>Forground</MenuItem>
+          <MenuItem value='Backgroundt'>Background</MenuItem>
+        </Select>
+      </FormControl>
+
       <Button
         variant='contained'
-        onClick={() => props.apphandleDropDown(choice)}
+        onClick={() =>
+          props.getImageFile({
+            movieTitle: movie.value,
+            colorMetric: colorMetric,
+            frameType: frameType,
+          })
+        }
       >
         Submit
       </Button>
